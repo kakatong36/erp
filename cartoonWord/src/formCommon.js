65a8038087bc02369482d4e1f13cb3565676b0e7
kakatong36 <1742293631@qq.com>
2017-12-10 18:17:53 +0800

    身份证验证

--- cartoonWord/src/formCommon.js
+++ cartoonWord/src/formCommon.js
@@ -110,6 +110,94 @@ const formCommon = {
             callback(new Error('数值不可为空'));
         }
     },
+    isIdcard: (rule, value, callback) =>{
+        if(value){
+            let gets = value,
+                decide;
+            var Wi = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]; // 加权因子;
+            var ValideCode = [1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2]; // 身份证验证位值，10代表X;
+            if (gets.length == 15) {
+                isValidityBrithBy15IdCard(gets);
+            }else if (gets.length == 18) {
+                var a_idCard = gets.split(''); // 得到身份证数组
+                if(isTrueValidateCodeBy18IdCard(a_idCard) && isValidityBrithBy18IdCard(gets)){
+                    decide = true
+                }else{
+                    decide = false
+                }
+            }    
+            
+            function isTrueValidateCodeBy18IdCard(a_idCard) {
+                var sum = 0; // 声明加权求和变量   
+                if (a_idCard[17].toLowerCase() == 'x') {
+                    a_idCard[17] = 10; // 将最后位为x的验证码替换为10方便后续操作   
+                }
+                for (var i = 0; i < 17; i++) {
+                    sum += Wi[i] * a_idCard[i]; // 加权求和
+                }
+                let valCodePosition = sum % 11; // 得到验证码所位置
+                if (a_idCard[17] == ValideCode[valCodePosition]) {
+                    return true
+                }else{
+                    return false
+                }
+            }
+            
+            function isValidityBrithBy18IdCard(idCard18) {
+                var year = idCard18.substring(6, 10);
+                var month = idCard18.substring(10, 12);
+                var day = idCard18.substring(12, 14);
+                var temp_date = new Date(year, parseFloat(month) - 1, parseFloat(day));
+                // 这里用getFullYear()获取年份，避免千年虫问题   
+                if (temp_date.getFullYear() != parseFloat(year) || temp_date.getMonth() != parseFloat(month) - 1 || temp_date.getDate() != parseFloat(day)) {
+                    return false
+                }else{
+                    return true
+                }
+            }
+            
+            function isValidityBrithBy15IdCard(idCard15) {
+                var year = idCard15.substring(6, 8);
+                var month = idCard15.substring(8, 10);
+                var day = idCard15.substring(10, 12);
+                var temp_date = new Date(year, parseFloat(month) - 1, parseFloat(day));
+                // 对于老身份证中的你年龄则不需考虑千年虫问题而使用getYear()方法  
+                console.log(isValidityProvice(idCard15));
+                if(isValidityProvice(idCard15)){
+                    if (temp_date.getYear() != parseFloat(year) || temp_date.getMonth() != parseFloat(month) - 1 || temp_date.getDate() != parseFloat(day)) {
+                        return decide = false;
+                    }else{
+                       return decide = true; 
+                    }
+                }
+            }
+            function isValidityProvice(val){
+                var vcity={ 11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古", 
+                    21:"辽宁",22:"吉林",23:"黑龙江",31:"上海",32:"江苏", 
+                    33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南", 
+                    42:"湖北",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆", 
+                    51:"四川",52:"贵州",53:"云南",54:"西藏",61:"陕西",62:"甘肃", 
+                    63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外" 
+                };
+                let provinceCode = val.substring(0,2);
+                if(vcity[provinceCode] != undefined){
+                    return true
+                }else{
+                    return false
+                }
+
+            }
+            if(!decide){
+                callback(new Error('请输入正确的身份证号'));
+            }else{
+                callback ()
+            }
+        }else if(!value && rule.required){
+            callback(new Error('身份证号不可为空'));
+        }else{
+            callback ()
+        }
+    },
     getAreaList: () =>{
         let provinceList = {};
         let cityList = {};
